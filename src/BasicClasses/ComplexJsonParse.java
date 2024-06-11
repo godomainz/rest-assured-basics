18a53025dd9636dbaa056ffd1d1a72a364b15c19
Akila Mendis <akila@oneit.com.au>
2024-06-11 17:34:25 +0530

    25. Understanding Structure of Complex Nested Json and its array notations

---
+++ src/BasicClasses/ComplexJsonParse.java
@@ -0,0 +1,13 @@
+package BasicClasses;
+
+import Files.Payload;
+import io.restassured.path.json.JsonPath;
+
+public class ComplexJsonParse {
+
+	public static void main(String[] args) {
+		JsonPath js = new JsonPath(Payload.coursePrice());
+
+	}
+
+}
