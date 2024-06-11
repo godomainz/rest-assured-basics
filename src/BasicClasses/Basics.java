b1a1be54f75228f9b48bc55a73bd8db821791258
Akila Mendis <akila@oneit.com.au>
2024-06-11 11:28:51 +0530

    21. Parsing the Json Response body using JsonPath class

--- src/BasicClasses/Basics.java
+++ src/BasicClasses/Basics.java
@@ -1,6 +1,7 @@
 package BasicClasses;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import Files.Payload;
@@ -8,12 +9,15 @@ import Files.Payload;
 public class Basics {
 	public static void main(String[] args) {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body(Payload.addPlace())
 		.when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		
-		// Add place -> Update Place with New Address -> Get Place to validate if New address is present in response
+		System.out.println(response);
+		JsonPath js = new JsonPath(response);
+		String placeId = js.getString("place_id");
+		System.out.println(placeId);
 		
 	}
 
