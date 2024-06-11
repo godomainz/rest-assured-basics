7329ca510f58f37d47c205f02b7ef8fd418d5f28
Akila Mendis <akila@oneit.com.au>
2024-06-11 11:11:53 +0530

    20. Assertions on Json Response Body and Headers through Automated code

--- src/BasicClasses/Basics.java
+++ src/BasicClasses/Basics.java
@@ -2,29 +2,18 @@ package BasicClasses;
 
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import Files.Payload;
 
 public class Basics {
 	public static void main(String[] args) {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.addPlace())
 		.when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		
+		// Add place -> Update Place with New Address -> Get Place to validate if New address is present in response
 		
 	}
 
