e7e1301fc8b87ed14fd1836a773090c4e99516c9
Akila Mendis <akila@oneit.com.au>
2024-06-11 16:20:31 +0530

    24. Importance of Junit/TestNG Assertions in validating the responses

--- src/BasicClasses/Basics.java
+++ src/BasicClasses/Basics.java
@@ -4,7 +4,11 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
 import Files.Payload;
+import Files.ReusableMethods;
 
 public class Basics {
 	public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class Basics {
 		.then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		
 		System.out.println(response);
-		JsonPath js = new JsonPath(response);
+		JsonPath js = ReusableMethods.rawToJson(response);
 		String placeId = js.getString("place_id");
 		System.out.println(placeId);
 		
@@ -32,9 +36,10 @@ public class Basics {
 		.when().get("maps/api/place/get/json")
 		.then().assertThat().statusCode(200).extract().response().asString();
 		
-		JsonPath getPlaceJs = new JsonPath(getPlaceResponse);
+		JsonPath getPlaceJs = ReusableMethods.rawToJson(getPlaceResponse);;
 		String actualAddress = getPlaceJs.getString("address");
 		System.out.println(actualAddress);
+		Assert.assertEquals(actualAddress, newAddress);
 	}
 
 
