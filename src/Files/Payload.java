59996ab2f964605ec7a3674894d0e9bd39ccbfd7
Akila Mendis <akila@oneit.com.au>
2024-06-12 10:38:34 +0530

    35. Sending parameters to payload from Test

--- src/Files/Payload.java
+++ src/Files/Payload.java
@@ -75,4 +75,14 @@ public class Payload {
 				+ "\r\n"
 				+ "}";
 	}
+	
+	public static String addBook(String isbn, String aisle) {
+		String payload = "{\r\n"
+				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
+				+ "\"isbn\":\""+isbn+"\",\r\n"
+				+ "\"aisle\":\""+aisle+"\",\r\n"
+				+ "\"author\":\"John foe\"\r\n"
+				+ "}";
+		return payload;
+	}
 }
