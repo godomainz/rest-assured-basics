db3a7df53eaa5d8fec8c5e2478e1d505bce694b3
Akila Mendis <akila@oneit.com.au>
2024-06-12 11:06:33 +0530

    37. Example on Parameterization of API Tests with multiple data sets

--- src/BasicClasses/DynamicJson.java
+++ src/BasicClasses/DynamicJson.java
@@ -1,28 +1,31 @@
 package BasicClasses;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import Files.Payload;
 import Files.ReusableMethods;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-
 import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
+	final String dataProviderName = "BooksData";
 	
-	@Test
-	private void addBook() {
+	@Test(dataProvider = dataProviderName)
+	private void addBook(String isbn, String aisle) {
 		RestAssured.baseURI = "http://216.10.245.166";
-		String response = given().log().all().header("Content-Type","application/json").body(Payload.addBook("bcd1","2271"))
+		String response = given().log().all().header("Content-Type","application/json").body(Payload.addBook(isbn,aisle))
 		.when().post("/Library/Addbook.php")
 		.then().log().all().assertThat().statusCode(200)
 		.extract().response().asString();
 		
 		JsonPath js = ReusableMethods.rawToJson(response);
 		String id = js.get("ID");
-		
+	}
+	
+	@DataProvider(name=dataProviderName)
+	private Object[][] getData() {
+		return new Object[][] {{"wqe","123"}, {"asds","432"}, {"zxxz","6575"}};
 
 	}
-
 }
