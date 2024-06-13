5368112e9644528055c80255793a6e60b68a5c0e
Akila Mendis <akila@oneit.com.au>
2024-06-13 10:38:07 +0530

    58. Rest Assured automation script for OAuth end to end API's test

---
+++ src/OAuth2/OAuthTest.java
@@ -0,0 +1,32 @@
+package OAuth2;
+import static io.restassured.RestAssured.*;
+
+import Files.ReusableMethods;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class OAuthTest {
+
+	public static void main(String[] args) {
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		
+		String client_id = "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com";
+		String client_secret = "erZOWM9g3UtwNRj340YYaK_W";
+		String grant_type = "client_credentials";
+		String scope = "trust";
+		
+		String response = given().log().all().formParams("client_id",client_id).formParams("client_secret", client_secret).formParams("grant_type", grant_type).formParams("scope", scope)
+		.when().post("/oauthapi/oauth2/resourceOwner/token").asString();
+		
+		
+		JsonPath js = ReusableMethods.rawToJson(response);
+		String access_token = js.getString("access_token");
+		
+		String courceDetails = given().queryParams("access_token",access_token)
+		.when().get("/oauthapi/getCourseDetails").asString();
+		
+		System.out.println(courceDetails);
+
+	}
+
+}
